c1041efbc58e30144361ba5258290bfc8306ae6b
Rafael santos <rp1437508@gmail.com>
2025-05-29 14:49:47 -0300

    primeiro commit do projeto

---
+++ cypress/e2e/WalkdogSubmit.cy.js
@@ -0,0 +1,95 @@
+/// <reference types="cypress" />
+
+describe('Formulario de cadastro Walkdog', () => {
+
+  let data;
+  beforeEach(() => {
+    cy.visit('https://walkdog.vercel.app/signup');
+    cy.get('form h1').should('have.text','Faça seu cadastro');
+  });
+ 
+  before(() => {
+    cy.fixture('candidate').then(candidate => {  data = candidate})
+  });
+
+
+  context('Dados', () => {
+
+    it('Deve preencher a seção DADOS', () => {
+    
+      cy.Type_text('name', data.personal.name);
+      cy.Type_text('email', data.personal.email);
+      cy.Type_text('cpf', data.personal.cpf);
+
+      cy.Contains_value('name', data.personal.name);
+      cy.Contains_value('email', data.personal.email);
+      cy.Contains_value('cpf', data.personal.cpf);
+
+    });
+
+  });
+
+  context('Endereço', () => {
+
+    it('Deve preencher a seção ENDEREÇO', () => {
+
+      cy.Type_text('cep', data.personal.cep);
+      cy.Button_click('Buscar CEP');
+      
+      cy.Type_text('addressNumber', data.personal.addressNumber);
+      cy.Type_text('addressDetails', data.personal.addressDetails);
+
+      cy.Contains_value('cep', data.personal.cep);
+      cy.Contains_value('addressStreet', data.personal.addressStreet);
+      cy.Contains_value('addressNumber', data.personal.addressNumber);
+      cy.Contains_value('addressDetails', data.personal.addressDetails);
+      cy.Contains_value('addressDistrict', data.personal.addressDistrict);
+      cy.Contains_value('addressCityUf', data.personal.addressCityUf);
+
+    });
+
+  });
+
+  context('Atividade extras', () => {
+
+    it('Deve escolher ao menos uma atividade', () => {
+
+      cy.Extra_skil(data.personal.activity2);
+      cy.Flag_option(data.personal.activity2);
+
+    });
+
+  });
+
+  context('Anexar arquivo', () => {
+
+    it('Deve anexar um aqrquivo', () => {
+
+      cy.Fixe_file('serj.jpeg');
+    });
+  });
+
+  context('submit form', () => {
+
+    it.only('Deve finalizar todo o cadastro', () => {
+
+      cy.Type_text('name', data.personal.name);
+      cy.Type_text('email', data.personal.email);
+      cy.Type_text('cpf', data.personal.cpf);
+
+      cy.Type_text('cep', data.personal.cep);
+      cy.Button_click('Buscar CEP');
+
+      cy.Type_text('addressNumber', data.personal.addressNumber);
+      cy.Type_text('addressDetails', data.personal.addressDetails);
+
+      cy.Extra_skil(data.personal.activity2);
+
+      cy.Fixe_file('serj.jpeg');
+
+      cy.Submit_form('Cadastrar');
+      cy.noticeSubmit('Obrigado!');
+    });
+  });
+
+})
---
+++ cypress/support/e2e.js
@@ -0,0 +1,58 @@
+// ***********************************************************
+// This example support/e2e.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+
+Cypress.Commands.add('Type_text', (name, type) => {
+
+    cy.get(`input[name=${name}]`).type(type);
+});
+
+Cypress.Commands.add('Button_click', (nameButton) => {
+
+    cy.get(`input[type="button"][value="${nameButton}"]`).click();
+});
+
+Cypress.Commands.add('Contains_value', (name, value) => {
+
+    cy.get(`input[name=${name}]`).should('have.value', value);
+});
+
+Cypress.Commands.add('Extra_skil', (nameSkil) => {
+
+    cy.get('.walker-service').parent().contains('span',nameSkil).click();
+});
+
+Cypress.Commands.add('Flag_option', (nameSkil) => {
+
+    cy.get(`.selected img[alt=${nameSkil}]`).should('have.css','border','0px none rgb(52, 130, 130)');
+});
+
+Cypress.Commands.add('Fixe_file', (file) => {
+
+    cy.get('input[accept="image/*"]').selectFile(`cypress/img/${file}`, {force: true})
+});
+
+Cypress.Commands.add('Submit_form', (nameButton) => {
+
+    cy.contains('button[type=submit]', nameButton).click();
+});
+
+Cypress.Commands.add('noticeSubmit', (noticeText) => {
+
+    cy.contains('h2', noticeText).should('be.visible');
+});
